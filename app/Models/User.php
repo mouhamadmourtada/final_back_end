9a4c4af88c28e8db33ede0afcb1ed868aa8a7282
Cheikh GAYE <gayec270198@gmail.com>
2024-05-16 23:54:28 +0000

    gestion des roles et permissions & morph

--- app/Models/User.php
+++ app/Models/User.php
@@ -64,4 +64,9 @@ class User extends Authenticatable implements JWTSubject
     {
         return [];
     }
+
+    public function fichiers()
+    {
+        return $this->morphMany(Fichier::class, 'fichierable');
+    }
 }
