dd5f98a1708bee72e7b85a349e80a90b768f433f
saliougit <username@users.noreply.github.com>
2024-05-17 16:36:51 +0000

    Routes chauffeur faits

--- database/seeders/PermissionsSeeder.php
+++ database/seeders/PermissionsSeeder.php
@@ -19,25 +19,25 @@ class PermissionsSeeder extends Seeder
         app()[PermissionRegistrar::class]->forgetCachedPermissions();
 
         // create permissions
-        Permission::create(['name' => 'create']);
-        Permission::create(['name' => 'read']);
-        Permission::create(['name' => 'update']);
-        Permission::create(['name' => 'delete']);
+        // Permission::create(['name' => 'create']);
+        // Permission::create(['name' => 'read']);
+        // Permission::create(['name' => 'update']);
+        // Permission::create(['name' => 'delete']);
 
-        // create roles
-        $role1 = Role::create(['name' => 'admin']);
-        $role2 = Role::create(['name' => 'client']);
-        $role3 = Role::create(['name' => 'chauffeur']);
+        // // create roles
+        // $role1 = Role::create(['name' => 'admin']);
+        // $role2 = Role::create(['name' => 'client']);
+        // $role3 = Role::create(['name' => 'chauffeur']);
         
-        // assign permissions to roles
-        $role1->givePermissionTo('create');
-        $role1->givePermissionTo('read');
-        $role1->givePermissionTo('update');
-        $role1->givePermissionTo('delete');
+        // // assign permissions to roles
+        // $role1->givePermissionTo('create');
+        // $role1->givePermissionTo('read');
+        // $role1->givePermissionTo('update');
+        // $role1->givePermissionTo('delete');
 
-        $role2->givePermissionTo('read');
+        // $role2->givePermissionTo('read');
 
-        $role3->givePermissionTo('read');
+        // $role3->givePermissionTo('read');
         // create a user
         $user = \App\Models\User::factory()->create([
             'nom' => 'User',
@@ -48,10 +48,7 @@ class PermissionsSeeder extends Seeder
             'telephone' => '555-555-5555',
             'date_naissance' => '1990-01-01',
             'lieu_naissance' => 'Paris',
-            'marque' => 'Toyota',
-            'matricule' => 'AA123AA',
-            'cin' => 'AA123456',
-            'senegalais_id' => '1',
+            'senegalais_id' => '1'
         ]);
 
         // assign roles to user
@@ -60,15 +57,13 @@ class PermissionsSeeder extends Seeder
         // create a user
         $user = \App\Models\User::factory()->create([
             'nom' => 'Gaye',
-            'prenom' => 'User',
+            'prenom' => 'Cheikh',
             'email' => 'gaye@example.com',
             'password' => Hash::make('password'),
             'adresse' => '123 Main St',
             'telephone' => '555-555-5555',
             'date_naissance' => '1990-01-01',
             'lieu_naissance' => 'Paris',
-            'marque' => 'Mercedes',
-            'matricule' => 'AA183AA',
             'cin' => 'A8123456',
             'senegalais_id' => '2',
         ]);
@@ -78,8 +73,8 @@ class PermissionsSeeder extends Seeder
 
         // create a user
         $user = \App\Models\User::factory()->create([
-            'nom' => 'Super',
-            'prenom' => 'Admin',
+            'nom' => 'Mbaye',
+            'prenom' => 'Chauffeur',
             'email' => 'super@example.fr',
             'password' => Hash::make('password'),
             'adresse' => '123 Main St',
